6d44413ca261ddba18fe609a7a1d2c813e1d6683
Yoichi-KIKUCHI <yoichi_kikuchi@r.recruit.co.jp>
2015-04-22 19:13:39 +0900

    Add the port assignment options.

---
+++ src/main/java/com/yo1000/bluefairy/controller/rest/v1/ImageRestController.java
@@ -0,0 +1,47 @@
+package com.yo1000.bluefairy.controller.rest.v1;
+
+import com.yo1000.bluefairy.model.entity.docker.Image;
+import com.yo1000.bluefairy.model.entity.docker.ImageInspect;
+import com.yo1000.bluefairy.model.service.ImageService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by yoichi.kikuchi on 15/04/17.
+ */
+@RestController
+@RequestMapping("api/v1/image")
+public class ImageRestController {
+    @Resource
+    private ImageService imageService;
+
+    @RequestMapping(value = "", method = RequestMethod.GET,
+            produces = "application/json;charset=UTF-8",
+            consumes = "application/json")
+    public Image[] getItems() {
+        return this.getImageService().getImages();
+    }
+
+    @RequestMapping(value = "all", method = RequestMethod.GET,
+            produces = "application/json;charset=UTF-8",
+            consumes = "application/json")
+    public Image[] getItemsAll() {
+        return this.getImageService().getImagesAll();
+    }
+
+    @RequestMapping(value = "{id:(?!^all$).+}", method = RequestMethod.GET,
+            produces = "application/json;charset=UTF-8",
+            consumes = "application/json")
+    public ImageInspect getItem(@PathVariable String id, Model model) {
+        return this.getImageService().getImage(id);
+    }
+
+    protected ImageService getImageService() {
+        return imageService;
+    }
+}
