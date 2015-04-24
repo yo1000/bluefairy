3b0092fd7b7e65131592fe7720de6e9050c5fbe3
Yoichi-KIKUCHI <yoichi_kikuchi@r.recruit.co.jp>
2015-04-24 23:53:53 +0900

    Add a name format tests.

--- src/main/java/com/yo1000/bluefairy/controller/rest/v1/ContainerRestController.java
+++ src/main/java/com/yo1000/bluefairy/controller/rest/v1/ContainerRestController.java
@@ -1,13 +1,17 @@
 package com.yo1000.bluefairy.controller.rest.v1;
 
+import com.yo1000.bluefairy.exception.IllegalRegexFormatException;
 import com.yo1000.bluefairy.model.entity.docker.Container;
 import com.yo1000.bluefairy.model.entity.docker.ContainerCreate;
 import com.yo1000.bluefairy.model.entity.docker.ContainerCreated;
 import com.yo1000.bluefairy.model.service.ContainerService;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.IllegalFormatConversionException;
+import java.util.IllegalFormatException;
 
 /**
  * Created by yoichi.kikuchi on 15/04/17.
@@ -38,11 +42,10 @@ public class ContainerRestController {
             consumes = "application/json")
     public ContainerCreated postCreate(@RequestBody ContainerCreate containerCreate,
                                        @RequestParam String name) {
+        Assert.isTrue(name.isEmpty() || name.matches("^/?[a-zA-Z0-9_-]+$"), "Name format is invalid.");
+
         return this.getContainerService().runContainer(containerCreate, name);
     }
-
-    //    @RequestMapping("{id:(?!^all$).+}")
-
 
     protected ContainerService getContainerService() {
         return containerService;
